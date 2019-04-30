6426aa2aef47480c3fe66f9ca1916b4b79010bf5
Eric <eshapiro@inphi.com>
2019-04-30 15:37:25 -0700

    Refactored and added some topology test cases

---
+++ topology/tests/test_TopSpace.py
@@ -0,0 +1,76 @@
+import pytest
+
+from ..topology import TopSpace
+
+
+def test_create_empty_topology():
+    X = TopSpace(set(), [set()])
+    assert isinstance(X, TopSpace)
+    assert X.space == frozenset()
+    assert X.open_sets == {frozenset()}
+
+def test_create_singleton_topology():
+    X = TopSpace({1}, [set(), {1}])
+    assert isinstance(X, TopSpace)
+    assert X.space == frozenset({1})
+    assert X.open_sets == frozenset({frozenset(), frozenset({1})})
+
+def test_create_small_topology():
+    space = {1, 2, 3}
+    open_sets = [
+        set(),
+        space,
+        {1}, {2}, {3},
+        {1, 2}, {2, 3}, {1, 3},
+    ]
+    X = TopSpace(space, open_sets)
+    assert isinstance(X, TopSpace)
+    assert X.space == frozenset(space)
+    assert X.open_sets == frozenset(map(frozenset, open_sets))
+
+def test_create_topology_missing_empty_set():
+    with pytest.raises(ValueError):
+        X = TopSpace({1}, [{1}])
+
+def test_create_topology_missing_space():
+    with pytest.raises(ValueError):
+        X = TopSpace({1}, [set()])
+
+def test_create_topology_not_closed_under_unions():
+    with pytest.raises(ValueError):
+        X = TopSpace({1, 2, 3}, [set(), {1, 2, 3}, {1}, {2}])
+
+def test_create_topology_not_closed_under_intersections():
+    with pytest.raises(ValueError):
+        X = TopSpace({1, 2, 3}, [set(), {1, 2, 3}, {1, 2}, {2, 3}])
+
+def test_create_topology_open_sets_not_subsets_of_space():
+    with pytest.raises(ValueError):
+        X = TopSpace(set(), [set(), {1}])
+
+def test_create_topology_from_subbasis():
+    space = set(range(3))
+    subbasis = [{i} for i in space]
+    X = TopSpace.from_subbasis(space, subbasis)
+    assert X.space == frozenset(space)
+    assert all([subset in X.open_sets for subset in subbasis])
+
+def test_create_topology_from_invalid_subbasis():
+    space = set(range(3))
+    subbasis = []
+    with pytest.raises(Exception):
+        X = TopSpace.from_subbasis(space, subbasis)
+    subbasis = [{0}]
+    with pytest.raises(Exception):
+        X = TopSpace.from_subbasis(space, subbasis)
+    subbasis = [{100}]
+    with pytest.raises(Exception):
+        X = TopSpace.from_subbasis(space, subbasis)
+
+def test_create_topology_from_basis():
+    # TODO
+    pass
+
+def test_create_topology_from_invalid_basis():
+    # TODO
+    pass
---
+++ topology/topology.py
@@ -0,0 +1,269 @@
+'''Classes for topological spaces and functions between them'''
+
+import itertools
+
+from typing import (
+    AbstractSet,
+    Dict,
+    Iterable,
+    Tuple,
+    Union,
+)
+
+
+class TopSpace:
+    '''Topological space
+
+    Attributes:
+        open_sets (frozenset[frozenset]): the open sets in the topology
+        space (frozenset): the underlying space
+    '''
+
+    def __init__(self, space: Union[set, frozenset], open_sets: Iterable[AbstractSet]) -> None:
+        # Check that all open sets are of type set or frozenset
+        if not all([isinstance(x, (set, frozenset)) for x in open_sets]):
+            raise TypeError('all open sets must be of type set or frozenset')
+        # Check that the empty set is open
+        if not set() in open_sets:
+            raise ValueError('the empty set must be open')
+        # Check that the space itself is open
+        if not space in open_sets:
+            raise ValueError('the space itself must be open')
+        # Check that every open set is a subset of the space
+        if not all([open_set <= space for open_set in open_sets]):
+            raise ValueError('all open sets must be subsets of the space')
+        # Check pairwise unions
+        unions, set1, set2 = self.pairwise_unions(open_sets)
+        if not unions:
+            raise ValueError('{} and {} are open but their union {} is not'.format(
+                set1,
+                set2,
+                set1 | set2))
+        # Check pairwise intersections
+        intersections, set1, set2 = self.pairwise_intersections(open_sets)
+        if not intersections:
+            raise ValueError('{} and {} are open but their intersection {} is not'.format(
+                set1,
+                set2,
+                set1 & set2))
+        # Cast all open sets to frozensets so they are hashable
+        self.open_sets = frozenset(map(frozenset, open_sets))
+        self.space = frozenset(space)
+
+    def __mul__(self, other):
+        '''Given two topological spaces, return their product space'''
+        # Create the product space from the cartestian product of self.space and other.space
+        product_space = itertools.product(self.space, other.space)
+        # Create a basis for the product topology from the cartesian products of open sets
+        product_basis = [itertools.product(set1, set2)
+                         for set1 in self.open_sets
+                         for set2 in other.open_sets]
+        # Create a topology from the basis
+        product = TopSpace.from_basis(product_space, list(map(set, product_basis)))
+        return product
+
+    def __eq__(self, other) -> bool:
+        '''Check whether two topological spaces are equal'''
+        # Topological spaces are equal if their underlying spaces are equal
+        # and they have the same open sets
+        return self.space == other.space and self.open_sets == other.open_sets
+
+    def is_closed(self, subset: AbstractSet) -> bool:
+        '''Check whether a subset of a topological space is closed'''
+        # A set is closed if its complement is open
+        return self.space - subset in self.open_sets
+
+    @staticmethod
+    def pairwise_unions(subsets: Iterable[AbstractSet]) -> Tuple[bool, AbstractSet, AbstractSet]:
+        '''Check whether a collection of subsets is closed under pairwise unions'''
+        # Loop through all distinct pairs of sets in subsets
+        for set1, set2 in itertools.combinations(subsets, 2):
+            # Make sure their union is in subsets
+            if not set1 | set2 in subsets:
+                return False, set1, set2
+        return True, None, None
+
+    @staticmethod
+    def pairwise_intersections(
+            subsets: Iterable[AbstractSet]) -> Tuple[bool, AbstractSet, AbstractSet]:
+        '''Check whether a collection of subsets is closed under pairwise intersections'''
+        # Loop through all distinct pairs of sets in subsets
+        for set1, set2 in itertools.combinations(subsets, 2):
+            # Make sure their intersection is in subsets
+            if not set1 & set2 in subsets:
+                return False, set1, set2
+        return True, None, None
+
+    @staticmethod
+    def is_basis(space: AbstractSet, subsets: Iterable[AbstractSet]):
+        '''Check whether a collection of subsets is a basis for a topology on a set'''
+        # Check that every point in the space is in some subset
+        if not all([any([point in subset for subset in subsets]) for point in space]):
+            return False
+        # Loop through all distinct pairs of sets in subsets
+        for set1, set2 in itertools.combinations(subsets, 2):
+            # Compute their intersection
+            intersection = set1 & set2
+            # If the intersection is empty, we're fine
+            if not intersection:
+                continue
+            # Otherwise, we need to make sure every point in the intersection is
+            # contained in some basis subset which is contained in the intersection
+            if not any([point in subset and subset <= intersection
+                        for subset in subsets
+                        for point in intersection]):
+                return False
+        return True
+
+    @staticmethod
+    def is_subbasis(space: AbstractSet, subsets: Iterable[AbstractSet]):
+        '''Check whether a collection of subsets is a subbasis for a topology on a set'''
+        # Check that every point in the space is in some subset
+        if not all([any([point in subset for subset in subsets]) for point in space]):
+            return False
+        return True
+
+    @classmethod
+    def from_subbasis(cls, space: AbstractSet, subbase_subsets: Iterable[AbstractSet]):
+        '''Construct a topological space from a subbasis'''
+        # Check that all subsets are of type set or frozenset
+        if not all([isinstance(x, (set, frozenset)) for x in subbase_subsets]):
+            raise TypeError('all elements of the topology must be sets')
+        # Check that subbase_subsets is actually a subbasis
+        if not cls.is_subbasis(space, subbase_subsets):
+            raise ValueError('a subbasis must cover all points in the space')
+        # Add the empty set
+        subbase_subsets.append(set())
+        subbase_subsets = list(map(frozenset, subbase_subsets))
+        base_subsets = subbase_subsets[:]
+        # Loop through all distinct pairs of sets in subbase_subsets
+        for idx1, set1 in enumerate(subbase_subsets):
+            for offset, _ in enumerate(subbase_subsets[idx1:]):
+                intersection = frozenset(set1.intersection(*subbase_subsets[idx1:idx1 + offset]))
+                base_subsets.append(intersection)
+        # base_subsets is now a basis
+        top = cls.from_basis(space, base_subsets)
+        return top
+
+    @classmethod
+    def from_basis(cls, space: AbstractSet, base_subsets: Iterable[AbstractSet]):
+        '''Construct a topological space from a basis'''
+        # Check that all subsets are of type set or frozenset
+        if not all([isinstance(x, (set, frozenset)) for x in base_subsets]):
+            raise TypeError('all elements of the topology must be sets')
+        # Check that base_subsets is actually a basis
+        if not cls.is_basis(space, base_subsets):
+            raise ValueError('not a valid basis for a topology')
+        # Add the empty set
+        base_subsets.append(set())
+        base_subsets = list(map(frozenset, base_subsets))
+        subsets = base_subsets[:]
+        # Loop through all distinct pairs of sets in base_subsets
+        for idx1, set1 in enumerate(base_subsets):
+            for offset, _ in enumerate(base_subsets[idx1:]):
+                union = frozenset(set1.union(*base_subsets[idx1:idx1 + offset]))
+                subsets.append(union)
+        # Create a topology from subsets
+        top = cls(space, set(map(frozenset, subsets)))
+        return top
+
+
+class Function:
+    '''Function (mapping between sets)
+
+    Attributes:
+        mapping (dict): value, image pairs (basically {x: f(x) for x in domain})
+        domain (frozenset): the function's domain
+        codomain (frozenset): the function's codomain
+    '''
+
+    def __init__(self, mapping: Dict, domain: AbstractSet, codomain: AbstractSet) -> None:
+        # Check that all elements of the domain get mapped
+        if not all([point in mapping.keys() for point in domain.space]):
+            raise ValueError('all values in the domain must get mapped')
+        # Check that everything in the mapping is valid
+        for key, val in mapping.items():
+            if key not in domain.space:
+                raise ValueError('value {} is not in the domain'.format(key))
+            if val not in codomain.space:
+                raise ValueError('value {} is not in the codomain'.format(val))
+        self.mapping = mapping
+        self.domain = domain
+        self.codomain = codomain
+
+    def __call__(self, value):
+        '''Given a value, return its image under the function'''
+        try:
+            return self.mapping[value]
+        except:
+            raise ValueError('{} is not in the domain'.format(value))
+
+    def __matmul__(self, other):
+        '''Create a new function from the composition self . other'''
+        mapping = {k: self.mapping[other.mapping[k]] for k in other.mapping}
+        return Function(mapping, other.domain, self.codomain)
+
+    def __eq__(self, other):
+        '''Check if two functions are equal'''
+        return (self.domain == other.domain
+                and self.codomain == other.codomain
+                and self.mapping == other.mapping)
+
+    def inverse(self):
+        '''Create a new function which is the inverse of this one'''
+        if not self.is_bijective():
+            raise ValueError('this function is not invertible')
+        mapping = {v: k for k, v in self.mapping.items()}
+        return Function(mapping, self.codomain, self.domain)
+
+    def fiber(self, value):
+        '''Get the preimage of a single value in the codomain'''
+        if value not in self.codomain:
+            raise ValueError('{} is not in the codomain'.format(value))
+        fiber = frozenset(k for k in self.mapping if self.mapping[k] == value)
+        return fiber
+
+    def preimage(self, subset: AbstractSet):
+        '''Get the preimage of a subset of the codomain'''
+        if not subset <= self.codomain:
+            raise ValueError('{} if not a subset of the codomain'.format(subset))
+        preimage = set()
+        for value in subset:
+            preimage |= self.fiber(value)
+        return preimage
+
+    def image(self, subset: AbstractSet):
+        '''Take the image of a subset under the funciton'''
+        img = set()
+        for value in subset:
+            img |= self(value)
+        return img
+
+    def is_bijective(self):
+        '''Check whether the function is a bijection'''
+        # Check for surjectivity
+        if not all([v in self.mapping.values() for v in self.codomain.space]):
+            return False
+        # Check for injectivity
+        if not all([len(self.fiber(v)) == 1 for v in self.mapping.values()]):
+            return False
+        return True
+
+    def is_continuous(self):
+        '''Check whether a function between topological spaces is continuous'''
+        # Check that the preimage of any open set is open
+        return all([self.preimage(openset) in self.domain.subsets
+                    for openset in self.codomain.subsets])
+
+    def is_homeomorphism(self):
+        '''Check whether a function between topological spaces is a homeomorphism'''
+        # Check that the function is bijective
+        if not self.is_bijective():
+            return False
+        # Check that the function is continuous
+        if not self.is_continuous():
+            return False
+        # Check that the function's inverse is continuous
+        if not self.inverse().is_continuous():
+            return False
+        return True

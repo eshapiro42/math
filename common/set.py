4e4f431ffa01652f3c83ca64ff16744dbc2dbaa6
Eric Shapiro <eshapiro42@gmail.com>
2019-12-13 21:19:22 -0800

    Added some useful functions to Set()

--- common/set.py
+++ common/set.py
@@ -29,7 +29,26 @@ class Set(collections.MutableSet, collections.Hashable):
         return '{{{}}}'.format(', '.join(map(str, self.elements)))
     
     def __repr__(self):
-        return 'Set({})'.format(', '.join(map(repr, self.elements)))
+        return '{{{}}}'.format(', '.join(map(str, self.elements)))
+#         return 'Set({})'.format(', '.join(map(repr, self.elements)))
+
+    def __sub__(self, other):
+        return Set(*(self.elements.difference(other.elements)))
+    
+    def __add__(self, other):
+        return self.union(other)
+    
+    def difference(self, other):
+        return self - other
+
+    def union(self, other):
+        return Set(*(self.elements.union(other.elements)))
+    
+    def intersection(self, other):
+        return Set(*(self.elements.intersection(other.elements)))
+    
+    def issubset(self, other):
+        return self.elements.issubset(other.elements)
 
     @property
     def powerset(self):
@@ -42,3 +61,27 @@ class Set(collections.MutableSet, collections.Hashable):
     @classmethod
     def _from_iterable(cls, iterable):
         return cls(*iterable)
+    
+    @staticmethod
+    def unions(*sets):
+        union = Set()
+        for s in sets:
+            union = union + s
+        return union
+    
+    def is_sigma_algebra(self, X):
+        try:
+            # {} and X must be in any sigma algebra
+            assert Set() in self
+            assert X in self
+            # a sigma algebra is closed under complements
+            for s in self:
+                assert X - s in self
+            # a sigma algebra is closed under (countable) unions
+            for length in range(len(X)):
+                for combination in itertools.combinations(self, length):
+                    assert self.unions(Set(*combination)).issubset(self)
+        except AssertionError:
+            return False
+        else:
+            return True
